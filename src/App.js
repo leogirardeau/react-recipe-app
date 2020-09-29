00820eccf4949f983c53a5d0b967f946e8487c3f
leogirardeau <leogir.pro@gmail.com>
2020-09-29 15:10:46 +0200

    useEffect useState and API set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "c1fc2b59";
+  const APP_KEY = "3857bf66adacff010f9c152597679f4d";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
